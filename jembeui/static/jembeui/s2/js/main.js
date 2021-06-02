fa3a0a3802e1549d0ddc009b8c3d5d78ba1e47ee
Predrag Peranović <predrag.peranovic@blokkod.me>
2021-06-02 09:01:36 +0200

    add s2 page notifications and system-error

---
+++ jembeui/static/jembeui/s2/js/main.js
@@ -0,0 +1,42 @@
+window.addEventListener(
+  'jembeUpdatePage',
+  function (event) {
+    if (event.detail.isXUpdate) {
+      for ([cname, cdom] of Object.entries(event.detail.components)) {
+        window.jembeClient.walkComponent(cdom, jembeui_s2_init_mui)
+      }
+
+    } else {
+      window.jembeClient.walkDocument(jembeui_s2_init_mui)
+    }
+  }
+)
+
+function jembeui_s2_init_mui(el) {
+  if (el.classList.contains('mdc-snackbar')) {
+    // Snackbar
+    if (el._mdcSnackbar == undefined) {
+      el._mdcSnackbar = mdc.snackbar.MDCSnackbar.attachTo(el);
+      el._mdcSnackbar.timeoutMs = -1;
+    }
+    el._mdcSnackbar.open();
+  } else if (el.classList.contains('mdc-banner')) {
+    // Banner
+    el._mdcBanner = mdc.banner.MDCBanner.attachTo(el);
+    el._mdcBanner.timeoutMs = -1;
+    el._mdcBanner.open();
+  } else if (el.classList.contains('mdc-button') && el._mdcRipple === undefined) {
+    // Button Ripple
+    el._mdcRipple = mdc.ripple.MDCRipple.attachTo(el);
+  } else if (el.classList.contains('mdc-dialog') && el._mdcDialog === undefined) {
+    // Dialog
+    el._mdcDialog = mdc.dialog.MDCDialog.attachTo(el);
+    // system error
+    if (el.classList.contains('system-error')) {
+      el._mdcDialog.scrimClickAction = '';
+    }
+    // el._mdcDialog.listen('MDCDialog:opened', () => {
+    //   el._mdcDialog.layout();
+    // });
+  }
+}
