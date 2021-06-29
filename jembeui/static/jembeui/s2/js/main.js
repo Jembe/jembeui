9bff2dfd07df01dc6412d8faf1ef4aef3ae57f33
Predrag Peranović <predrag.peranovic@blokkod.me>
2021-06-29 16:02:42 +0200

    wip s2 navigation

--- jembeui/static/jembeui/s2/js/main.js
+++ jembeui/static/jembeui/s2/js/main.js
@@ -16,12 +16,9 @@ function jembeui_s2_init_mui(el) {
   if (el.classList.contains('mdc-snackbar')) {
     // Snackbar
     el._mdcSnackbar = mdc.snackbar.MDCSnackbar.attachTo(el);
-    // el._mdcSnackbar.timeoutMs = -1;
-    // el._mdcSnackbar.open();
   } else if (el.classList.contains('mdc-banner')) {
     // Banner
     el._mdcBanner = mdc.banner.MDCBanner.attachTo(el);
-    // el._mdcBanner.open();
   } else if (el.classList.contains('mdc-button') && el._mdcRipple === undefined) {
     // Button Ripple
     el._mdcRipple = mdc.ripple.MDCRipple.attachTo(el);
@@ -31,12 +28,21 @@ function jembeui_s2_init_mui(el) {
     if (el.classList.contains('scrim-click-disabled')) {
       el._mdcDialog.scrimClickAction = '';
     }
-    // el._mdcDialog.listen('MDCDialog:opened', () => {
-    //   el._mdcDialog.layout();
-    // });
   } else if (el.classList.contains('mdc-linear-progress')) {
     // linear progress bar
     el._mdcProgress = mdc.linearProgress.MDCLinearProgress.attachTo(el);
+  } else if (el.classList.contains('mdc-drawer') && el._mdcDrawer === undefined) {
+    // Dismisable drawer
+    el._mdcDrawer = mdc.drawer.MDCDrawer.attachTo(el);
+  } else if (el.classList.contains('mdc-top-app-bar') && el._mdcTopAppBar === undefined) {
+    // Top App Bar
+    el._mdcTopAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(el);
   }
+  // } else if (el.classList.contains('mdc-list') && el._mdcList === undefined) {
+  //   // List
+  //   el._mdcList = mdc.list.MDCList.attachTo(el);
+  //   el._mdcList.listElements.map(function(listItemEl){return new mdc.ripple.MDCRipple(listItemEl)})
+  // }
+
 
 }
