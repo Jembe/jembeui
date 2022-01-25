b16436ea922905854eca78cbc0098388dcc57dd1
Predrag Peranović <predrag.peranovic@blokkod.me>
2022-01-25 23:28:25 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,19 +26,38 @@ module.exports = {
             boxShadow: {
                 'focus': '0 0 0 1px #0ea5e9 inset, 0 0 0 1px #0ea5e9, 0 0 0 4px #0ea5e94d',
                 'focus-danger': '0 0 0 1px #e64f43 inset, 0 0 0 1px #e64f43, 0 0 0 4px #e64f434d',
+            },
+            animation: {
+                'progress': 'running-progress 2s cubic-bezier(0.4, 0, 0.2, 1) infinite'
+            },
+            keyframes: {
+                'running-progress': {
+                    '0%': {
+                        'margin-left': '0px', 
+                        'margin-right': '100%',
+                    },
+                    '50%': {
+                        'margin-left': '25%', 
+                        'margin-right': '0%',
+                    },
+                    '100%': {
+                        'margin-left': '100%', 
+                        'margin-right': '0',
+                    }
+                },
             }
-        },
+        }
     },
     variants: {
         extends: {
-            backgroundColor: ['disabled'],
+        backgroundColor: ['disabled'],
             textColor: ['disabled'],
         }
-    },
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms')({
-            strategy: "class"
-        }),
-    ],
+},
+plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')({
+        strategy: "class"
+    }),
+],
 }
