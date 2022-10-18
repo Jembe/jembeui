15bb7f770cfe85cc1dc783011c4c06318aae00c7
Predrag Peranović <predrag.peranovic@blokkod.me>
2022-10-18 15:01:57 +0200

    remove forms adabtable

--- jembeui/components/form/__init__.py
+++ jembeui/components/form/__init__.py
@@ -1,13 +1,5 @@
-from .form_base import *
-
-# fixed form with jembeui field support
 from .form import *
 from .create_record import *
 from .view_record import *
 from .update_record import *
 
-# adabtable form no jembeui field support
-from .form_adaptable import *
-from .create_record_adaptable import *
-from .view_record_adaptable import *
-from .update_record_adaptable import *
